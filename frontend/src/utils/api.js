de1c31539cbe5756cf8a77f3659e9943ae5c9e32
Ayush-Rautray <rautray.ayush03@gmail.com>
2026-02-13 12:53:23 +0530

    Fixing Api8

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,13 +1,26 @@
-import axios from "axios";
-const local_api = "http://localhost:5000";
-const production_api = "";
+// import axios from "axios";
+// const local_api = "http://localhost:5000";
+// const production_api = "";
 
-const token = localStorage.getItem("canva_token");
+// const token = localStorage.getItem("canva_token");
+
+// const api = axios.create({
+//   baseURL: local_api,
+//   headers: {
+//     Authorization: token ? `Bearar ${token}` : "",
+//   },
+//   withCredentials: true,
+// });
+
+// export default api;
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: local_api,
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
-    Authorization: token ? `Bearar ${token}` : "",
+    Authorization: localStorage.getItem("canva_token")
+      ? `Bearer ${localStorage.getItem("canva_token")}`
+      : "",
   },
   withCredentials: true,
 });
